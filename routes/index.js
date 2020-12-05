cad74a2c9017bdbf24c0ab32941ad91625b492cc
cutratefuture <christian@cutratefuture.com>
2020-12-04 17:23:56 -0800

    dummy route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Yarp' });
+  res.render('index', { title: 'Express Template' });
 });
+
+router.get('/page2', (req, res, next) => {
+  res.render('page2', { title: 'Express Template' });
+});
+
 
 module.exports = router;
